23fdc7e4b9901bbfe44df28f064eca169fed5451
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-12-01 23:29:39 +0100

    Add files via upload

---
+++ models/car.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+"github.com/google/uuid"
+)
+
+type Car struct {
+	ID 			uuid.UUID 	`json:"id"`
+	Name	    string		`json:"name"`
+	Year 	    string 		`json:"year"`
+	Brand 	    string 		`json:"brand"`
+	FuelType 	string 		`json:"fuel_type"`
+	Engine Engine 			`json:"engine"`
+	Price		float64		`json:"price"`
+	CreatedAt	time.Time	`json:"created_at"`
+	UpdatedAt	time.Time	`json:"updated_at"`
+}
+
+type CarRequest struct {
+	Name	    string		`json:"name"`
+	Year 	    string 		`json:"year"`
+	Brand 	    string 		`json:"brand"`
+	FuelType 	string 		`json:"fuel_type"`
+	Engine Engine 			`json:"engine"`
+	Price		float64		`json:"price"`
+}
+
+func Validate(name string) {
+
+}
