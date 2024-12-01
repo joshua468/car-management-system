32158c85d63fc116d24589d0e67e1ec7be4e6954
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-12-01 23:40:07 +0100

    Update car.go

--- models/car.go
+++ models/car.go
@@ -1,16 +1,20 @@
-package main
+package models
 
-import(
-"github.com/google/uuid"
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type Car struct {
-	ID 			uuid.UUID 	`json:"id"`
-	Name	    string		`json:"name"`
-	Year 	    string 		`json:"year"`
-	Brand 	    string 		`json:"brand"`
+	ID 		uuid.UUID       `json:"id"`
+	Name	        string		`json:"name"`
+	Year 	        string 		`json:"year"`
+	Brand 	        string 		`json:"brand"`
 	FuelType 	string 		`json:"fuel_type"`
-	Engine Engine 			`json:"engine"`
+	Engine 		Engine 		`json:"engine"`
 	Price		float64		`json:"price"`
 	CreatedAt	time.Time	`json:"created_at"`
 	UpdatedAt	time.Time	`json:"updated_at"`
@@ -20,11 +24,62 @@ type CarRequest struct {
 	Name	    string		`json:"name"`
 	Year 	    string 		`json:"year"`
 	Brand 	    string 		`json:"brand"`
-	FuelType 	string 		`json:"fuel_type"`
-	Engine Engine 			`json:"engine"`
-	Price		float64		`json:"price"`
+	FuelType    string 		`json:"fuel_type"`
+	Engine 	    Engine 		`json:"engine"`
+	Price	   float64		`json:"price"`
 }
 
-func Validate(name string) {
+func validate(name string)error {
+if name ==  ""{
+	return errors.New("Name is Required")
+}
+return nil
+}
+func validateYear(year string)error {
+if year == ""{
+	return errors.New("Year is Required")
+}
+_,err:=strconv.Atoi(year)
+if err!=nil{
+	return errors.New("year must be a valid number")
+}
+currentYear:= time.Now().Year()
+yearInt,_:= strconv.Atoi(year)
+if yearInt < 1886 || yearInt > currentYear {
+	return errors.New("Year must be between 1886 and current year")
+}
+return nil
+} 
 
+func validatedBrand(brand string)error {
+if brand == "" {
+	return errors.New("Brand is required")
+}
+return nil
+}
+
+func  validateFuelType(fuelType string)error {
+	validateFuelTypes:= []string{"Petrol","Diesel","Electric","Hybrid"}
+	for _,validType := range validateFuelTypes {
+		if fuelType == validType {
+			return nil
+		}
+	}
+	return errors.New("fuelType must be one of :Petrol,Diesel,Elctric,Hybrid")
+}
+
+func validateEngine(engine Engine)error {
+	if engine.EngineID == uuid.Nil {
+		return errors.New("EngineID is required")
+	}
+	if engine.Displacement <=0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	if engine.NoOfCylinders <=0 {
+		return errors.New("noOfCylinders must be greater than zero")
+	}
+	if engine.CarRange<=0 {
+		return errors.New("carRange must be greater than zero")
+	}
+	return nil
 }
