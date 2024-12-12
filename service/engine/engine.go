2c4aaed889e1ac0f1a54600c016122b09fc2e408
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-12-12 23:56:49 +0100

    Update engine.go

--- service/engine/engine.go
+++ service/engine/engine.go
@@ -1,1 +1,57 @@
 package engine
+
+import (
+	"context"
+
+	"github.com/joshua468/car-management-system/carZone/models"
+	"github.com/joshua468/car-management-system/carZone/store"
+)
+
+type EngineService struct {
+	store store.EngineStoreInterface
+}
+
+func NewEngineService(store store.EngineStoreInterface) *EngineService {
+	return &EngineService{
+		store: store,
+	}
+}
+
+func (s *EngineService) GetEngineByID(ctx context.Context, id string) (*models.Engine, error) {
+	engine, err := s.store.EngineById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &engine, nil
+}
+
+func (s *EngineService) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (*models.Engine, error) {
+	if err := models.ValidateEngineRequest(*engineReq); err != nil {
+		return nil, err
+	}
+
+	createdEngine, err := s.store.EngineCreate(ctx, engineReq)
+	if err != nil {
+		return nil, err
+	}
+	return &createdEngine, nil
+}
+
+func (s *EngineService) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (*models.Engine, error) {
+	if err := models.ValidateEngineRequest(*engineReq); err != nil {
+		return nil, err
+	}
+	updatedEngine, err := s.store.EngineUpdate(ctx, id, engineReq)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedEngine, nil
+}
+
+func (s *EngineService) DeleteEngine(ctx context.Context, id string) (*models.Engine, error) {
+	deletedEngine, err := s.store.EngineDelete(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &deletedEngine, nil
+}
